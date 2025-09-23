3273e5ec031d80974bda1b9c7e1e9b2bedd12c1b
khedr farrag <khedr.farrag@gmail.com>
2025-09-23 20:34:47 +0300

    updatenumber

--- src/components/public/home/ContactInfo.tsx
+++ src/components/public/home/ContactInfo.tsx
@@ -252,9 +252,9 @@ export default function ContactInfo() {
                         <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                       </svg>
                     </div>
-                    <Link href={"tel:+966 59 837 7921"}>
+                    <Link href={"tel:+966 50 611 3841"}>
                       <span className="text-gray-300 dark:text-gray-900  hover:text-[var(--color-primary)] transition-colors duration-400">
-                        +966 59 837 7921
+                      +966 50 611 3841
                       </span>
                     </Link>
                   </div>
@@ -291,7 +291,7 @@ export default function ContactInfo() {
                         </div>
                       </Link>
                       {/* WhatsApp */}
-                      <Link href={"https://wa.me/966598377921"}>
+                      <Link href={"https://wa.me/+966506113841"}>
                         <div className="w-9 h-9 bg-gradient-to-r from-green-500 to-green-600 dark:from-green-600 dark:to-green-700 rounded-full flex items-center justify-center cursor-pointer hover:from-green-600 hover:to-green-700 dark:hover:from-green-700 dark:hover:to-green-800 transition-all duration-200">
                           <i className="fa-brands fa-whatsapp text-white text-sm"></i>
                         </div>
